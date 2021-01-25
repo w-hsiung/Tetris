632b1c01abcc72a51b74da0df06d27c54a6f6882
WH <whsiung.my@gmail.com>
2021-01-25 08:37:37 +0800

    First commit.

---
+++ test.js
@@ -0,0 +1,13 @@
+let x = 20;
+
+switch (x) {
+
+    case 20:
+        console.log("You're 20");
+        break;
+    case 21:
+        console.log("You're 21");
+        break;
+    default:
+        
+}
